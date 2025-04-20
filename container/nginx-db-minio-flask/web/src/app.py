911d26750857d6dd71e79cd3b36d85b7ccab2f30
Ondřej Profant <ondrej.profant@gmail.com>
2025-04-20 11:14:37 +0200

    Containers: nginx-db-minio-flask example v1

---
+++ container/nginx-db-minio-flask/web/src/app.py
@@ -0,0 +1,15 @@
+from flask import Flask, render_template
+import os
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    env_vars = {
+        "FLASK_ENV":       os.getenv("FLASK_ENV", "-not set–"),
+        "FLASK_DEBUG":     os.getenv("FLASK_DEBUG", "–not set–"),
+        "POSTGRES_DB":     os.getenv("POSTGRES_DB", "–not set–"),
+        "MINIO_ROOT_USER": os.getenv("MINIO_ROOT_USER", "–not set–"),
+        "MINIO_BUCKET":    os.getenv("MINIO_BUCKET", "–not set–")
+    }
+    return render_template("index.html", env_vars=env_vars)
